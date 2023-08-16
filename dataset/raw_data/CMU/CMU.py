4299bc47e1c58e6fc22b87b85b7d7d50b08331a6
ZaitianWang <wangzaitian@outlook.com>
2023-08-16 10:15:42 +0800

    add CMU data processing

--- dataset/raw_data/CMU/CMU.py
+++ dataset/raw_data/CMU/CMU.py
@@ -16,7 +16,7 @@ def compute_life(c_capacity):
     return i
 
 
-def split_cycle(dataframe, ):
+def split_cycle(dataframe):
     """
     :param is_end: When it is true, only the last set of pkl_data in each cycle is recorded
     :param dataframe: raw pkl_data
@@ -27,15 +27,18 @@ def split_cycle(dataframe, ):
 
     cycle_number = np.array(dataframe['cycleNumber'])
     index = np.where(np.diff(cycle_number) == 1)[0] + 1
-    discharge_time, charge_time = [], []
+
+    discharge_time = []
     discharge_voltage = []
     discharge_current = []
     discharge_capacity = []
-    charge_capacity = []
-    charge_voltage = []
-    charge_temperature = []
-    charge_current = []
     discharge_temperature = []
+    charge_time = []
+    charge_voltage = []
+    charge_current = []
+    charge_capacity = []
+    charge_temperature = []
+
     all_time = np.array(dataframe['time_s'])
     all_charge_capacity = np.array(dataframe['QCharge_mA_h']) / 1000
     all_discharge_capacity = np.array(dataframe['QDischarge_mA_h']) / 1000
@@ -43,39 +46,37 @@ def split_cycle(dataframe, ):
     all_voltage = np.array(dataframe['Ecell_V'])
     all_temperature = np.array(dataframe['Temperature__C'])
     for j in range(len(index) - 1):
+        time = all_time[index[j]:index[j + 1]]
         c = all_current[index[j]:index[j + 1]]
         v = all_voltage[index[j]:index[j + 1]]
-        time = all_time[index[j]:index[j + 1]]
         c_capa = all_charge_capacity[index[j]:index[j + 1]]
         d_capa = all_discharge_capacity[index[j]:index[j + 1]]
         t = all_temperature[index[j]:index[j + 1]]
         indices = np.argmax(c < 0)
         if indices == 0:
             continue
+        charge_time.append(time[:indices])
         charge_voltage.append(v[:indices])
         charge_current.append(c[:indices])
+        charge_capacity.append(c_capa[:indices])
         charge_temperature.append(t[:indices])
-        discharge_voltage.append(v[indices:])
         discharge_time.append(time[indices:])
-        charge_time.append(time[:indices])
+        discharge_voltage.append(v[indices:])
         discharge_current.append(c[indices:])
+        discharge_capacity.append(d_capa[indices:])
         discharge_temperature.append(t[indices:])
 
-        discharge_capacity.append(d_capa[indices:])
-
-        charge_capacity.append(c_capa[:indices])
-
     battery = {
-        'charge_temperature': charge_temperature,
-        'charge_voltage': charge_voltage,
-        'charge_current': charge_current,
-        'charge_capacity': charge_capacity,
-
+        # 'charge_time': charge_time,
+        # 'charge_current': charge_current,
+        # 'charge_voltage': charge_voltage,
+        # 'charge_capacity': charge_capacity,
+        # 'charge_temperature': charge_temperature,
+        'discharge_time': discharge_time,
         'discharge_current': discharge_current,
+        'discharge_voltage':discharge_voltage,
+        'discharge_capacity':discharge_capacity,
         'discharge_temperature': discharge_temperature,
-        'discharge_capacity': discharge_capacity,
-        'discharge_voltage': discharge_voltage,
-
         'life': compute_life(c_capacity=charge_capacity),
         'charge_protocol': 'CCCV',
         'material': 'lithium-ion battery'}
@@ -84,14 +85,16 @@ def split_cycle(dataframe, ):
 
 if __name__ == '__main__':
     CMU_battery = {}
-    for i in tqdm(range(1, 30)):
+    # 
+    for i in tqdm(range(1, 30+1)):
+    # 
         try:
-            filename = 'VAH' + str(i).rjust(2, '0') + '.csv'
+            filename = './[CMU]14226830/VAH' + str(i).rjust(2, '0') + '.csv'
             df = pd.read_csv(filename)
         except FileNotFoundError:
             continue
         cycle = split_cycle(df)
         CMU_battery['CMU_battery_' + str(i)] = cycle
 
-    with open('../../pkl_data/CMU.pkl', 'wb') as f:
+    with open('./[CMU]14226830/pkl_data/CMU.pkl', 'wb') as f:
         pickle.dump(CMU_battery, f)
